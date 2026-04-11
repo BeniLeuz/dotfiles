80c4d9919467d7c87edcdc8c467f68ba2a968579
Benjamin Leuzinger <beleuz@gmx.ch>
2026-04-12 01:15:17 +0200

    tree sitter now fixed again

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,32 @@
-local configs = require("nvim-treesitter.configs")
+local ts = require("nvim-treesitter")
 
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-  pattern = "*.lhs",
-  callback = function()
-    vim.bo.filetype = "haskell"
-  end,
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "*.lhs",
+	callback = function()
+		vim.bo.filetype = "haskell"
+	end,
 })
-
-configs.setup({
-  highlight = { enable = true },
-  indent = { 
-    enable = true,
-    disable = { 'ruby' }
-  },
-  auto_install = true,
-})
-
 
 vim.filetype.add({
-  extension = {
-    html = "html",
-  },
+	extension = {
+		html = "html",
+	},
 })
 
-vim.treesitter.language.register("ruby", "crystal")
--- Make sure both languages are recognized
-vim.treesitter.language.register("gotmpl", "html")
+vim.treesitter.language.register("ruby", { "crystal" })
+vim.treesitter.language.register("gotmpl", { "html" })
+
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function(ev)
+		local lang = vim.treesitter.language.get_lang(ev.match)
+		local available_langs = require("nvim-treesitter").get_available()
+		local is_available = vim.tbl_contains(available_langs, lang)
+		if is_available then
+      if not vim.tbl_contains(require("nvim-treesitter").get_installed(), lang) then
+        vim.notify("Installing tree-sitter parser for " .. lang .. "...", vim.log.levels.INFO)
+      end
+			require("nvim-treesitter").install(lang):wait()
+			vim.treesitter.start()
+		end
+	end,
+})
