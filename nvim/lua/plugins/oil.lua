cb3048842f97c2044497ca9181f25c8a509d2621
Benjamin Leuzinger <beleuz@gmx.ch>
2026-04-15 00:38:05 +0200

    setting an oil keybind to copy to clipboard

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,13 +1,13 @@
 local oil = require("oil")
 oil.setup({
-  skip_confirm_for_simple_edits = true,
-  view_options = {
-    show_hidden = true
-  },
-  keymaps = {
-    ["<C-h>"] = {},
-  },
+	skip_confirm_for_simple_edits = true,
+	view_options = {
+		show_hidden = true,
+	},
+	keymaps = {
+		["<C-h>"] = {},
+		["<leader>oc"] = "actions.copy_to_system_clipboard",
+	},
 })
 
 vim.keymap.set("n", "<leader>pv", require("oil").open, { desc = "Open parent directory with Oil" })
-
